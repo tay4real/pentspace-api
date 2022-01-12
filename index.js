ba14895678026bbd025981101119d7099decf1fb
Otubusin Ademuyiwa <53128146+tay4real@users.noreply.github.com>
2022-01-12 16:54:03 +0100

    update

--- index.js
+++ index.js
@@ -1,52 +1,57 @@
-const express = require("express")
-const app = express()
-const mongoose = require("mongoose")
-const dotenv = require("dotenv")
+const express = require("express");
+const app = express();
+const mongoose = require("mongoose");
+const dotenv = require("dotenv");
 // const helmet = require("helmet")
 // const morgan = require("morgan")
-const userRoute = require("./routes/users")
-const authRoute = require("./routes/auth")
-const postRoute = require("./routes/posts")
-const categoryRoute = require("./routes/categories")
-const multer = require("multer")
+const userRoute = require("./routes/users");
+const authRoute = require("./routes/auth");
+const postRoute = require("./routes/posts");
+const healthcareServiceRoute = require("./routes/heathcareservices");
+const rating = require("./routes/ratings");
+const multer = require("multer");
 
 dotenv.config();
 
-
-mongoose.connect(process.env.MONGO_URL, {
+mongoose
+  .connect(process.env.MONGO_URL, {
     useNewURLParser: true,
     useUnifiedTopology: true,
-}).then(() => console.log("DBConnection Successful!")).catch((err) => {
-    console.log(err)
-});
+  })
+  .then(() => console.log("DBConnection Successful!"))
+  .catch((err) => {
+    console.log(err);
+  });
 
 const storage = multer.diskStorage({
-    destination: (req,file,cb) => {
-        cb(null, "images")
-    },filename: (req, file, cb) => {
-        cb(null, req.body.name)
-    }
-})
+  destination: (req, file, cb) => {
+    cb(null, "images");
+  },
+  filename: (req, file, cb) => {
+    cb(null, req.body.name);
+  },
+});
 
-const upload = multer({storage: storage})
-app.post("/api/upload", upload.single("file"), (req,res) => {
-    res.status(200).json("file has been uploaded")
-})
+const upload = multer({ storage: storage });
+app.post("/api/upload", upload.single("file"), (req, res) => {
+  res.status(200).json("file has been uploaded");
+});
 
 // Middleware
-app.use(express.json())
+app.use(express.json());
 // app.use(helmet())
 // app.use(morgan("common"))
 
-app.get("/", (req,res) => {
-    res.send("Welcome to Pentspace API")
-})
-app.use("/api/auth", authRoute); 
-app.use("/api/users", userRoute); 
-app.use("/api/posts", postRoute); 
-app.use("/api/categories", categoryRoute); 
+app.get("/", (req, res) => {
+  res.send("Welcome to Pentspace API");
+});
+app.use("/api/auth", authRoute);
+app.use("/api/users", userRoute);
+app.use("/api/posts", postRoute);
+app.use("/api/healthcareservices", healthcareServiceRoute);
+app.use("/api/healthserviceratings", rating);
 
-let port = process.env.PORT || 5000
-app.listen(port , () => {
-    console.log("Backend server is running on port " + port);
-}) 
+let port = process.env.PORT || 5000;
+app.listen(port, () => {
+  console.log("Backend server is running on port " + port);
+});
---
+++ routes/ratings.js
@@ -0,0 +1,77 @@
+const router = require("express").Router();
+const HealthServiceRating = require("../models/HealthServiceRating");
+const User = require("../models/User");
+
+//create a rating
+router.post("/", async (req, res) => {
+  const newRating = new HealthServiceRating(req.body);
+  try {
+    const savedRating = await newRating.save();
+    console.log(savedRating);
+    res.status(200).json(savedRating);
+  } catch (error) {
+    res.status(500).json(error);
+    console.log(error);
+  }
+});
+
+//update a rating
+router.put("/:id", async (req, res) => {
+  try {
+    const rating = await HealthServiceRating.findById(req.params.id);
+    if (rating.userId === req.body.userId) {
+      await rating.updateOne({ $set: req.body });
+      res.status(200).json("The rating has been updated ");
+    } else {
+      res.status(403).json("You can only update your rating");
+    }
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
+//delete a rating
+router.delete("/:id", async (req, res) => {
+  try {
+    const rating = await HealthServiceRating.findById(req.params.id);
+    if (rating.userId === req.body.userId) {
+      await post.deleteOne();
+      res.status(200).json("The rating has been deleted");
+    } else {
+      res.status(403).json("You can only delete your rating");
+    }
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
+//get a rating
+router.get("/:id", async (req, res) => {
+  try {
+    const rating = await HealthServiceRating.findById(req.params.id);
+    res.status(200).json(post);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
+//get timeline comments
+router.get("/timeline/all", async (req, res) => {
+  try {
+    const currentUser = await User.findById(req.body.userId);
+
+    const userRatings = await HealthServiceRating.find({
+      userId: currentUser._id,
+    });
+    const friendRatings = await Promise.all(
+      currentUser.following.map((friendId) => {
+        return HealthServiceRating.find({ userId: friendId });
+      })
+    );
+    res.json(userRatings.concat(...friendRatings));
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
+module.exports = router;
